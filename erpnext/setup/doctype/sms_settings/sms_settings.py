646cbac418a41fb5af3633d9aa45af5eb5001f71
pawaranand <pawaranand00@gmail.com>
2015-12-26 15:45:30 +0530

    [Fix]Sms Log

--- erpnext/setup/doctype/sms_settings/sms_settings.py
+++ erpnext/setup/doctype/sms_settings/sms_settings.py
@@ -119,4 +119,4 @@ def create_sms_log(args, sent_to):
 	sl.requested_numbers = "\n".join(args['receiver_list'])
 	sl.no_of_sent_sms = len(sent_to)
 	sl.sent_to = "\n".join(sent_to)
-	sl.save()
+	sl.save(ignore_permissions=True)
